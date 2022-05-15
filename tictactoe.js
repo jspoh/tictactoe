2a67dcf81c3808e75e66cea2d8587f729c8276a8
js <99598950+jspoh@users.noreply.github.com>
2022-05-15 19:26:04 +0800

    Add files via upload

--- tictactoe.js
+++ tictactoe.js
@@ -1,36 +1,144 @@
 //140522 started making this to learn minimax algorithms
-//150522 done, although AI seems to put a higher emphasis on not losing instead of winning
+//150522 done, although AI seems to put a higher emphasis on not losing instead of winning. i guess thats normal?
 
-const playerIconCode = '&#9675;';
+//const playerIconCode = '&#10006;';
 const playerIcon = '○';
-const aiIconCode =  '&#9587;';
-const aiIcon = '╳';
+//const aiIconCode =  '&#9587;';
+const aiIcon = '✖';
 let board = []; for (let i=0; i<9; i++) {board.push('');}
 let winner;
 const AllBtns = document.querySelectorAll('.box');
-const body = document.querySelector('body');
+const container = document.querySelector('.container');
+const playerBtn = document.querySelector('#player');
+const aiBtn = document.querySelector('#ai');
+const pvpBtn = document.querySelector('#pvp');
+const retryText = document.querySelector('#retry');
 
 setInterval(()=>{ //game loop?
     updateUiFromBoard(); //update visuals
 
     winner = gameWinner(); //check if anyone has won/lost/draw
-    if (winner === 'player') {body.style.backgroundColor = 'green';} 
-    else if (winner === 'ai') {body.style.backgroundColor = 'red';} 
-    else if (winner === 'draw') {body.style.backgroundColor = 'blue';} 
+    if (winner !== null) {
+        showRetry(winner);
+        if (winner === 'player') {container.style.backgroundColor = 'green';} 
+        else if (winner === 'ai') {container.style.backgroundColor = 'red';} 
+        else if (winner === 'draw') {container.style.backgroundColor = 'cadetblue';} 
+    }
 
-    //good stuff
+    //check if AI moves first has been selected
+    aiMoveFirst();
 }, 10)
 
+let gameTurn = 1;
 for (let btn of AllBtns) {
     btn.addEventListener('click', (e)=>{
-        if (e.srcElement.innerText === '') {
-            board[parseInt(e.target.id[1])] = playerIcon;
-            
-            //minimax stuff
-            bestMove(board);
+        if (winner === null && gameMode !== 'pvp') {
+            if (e.srcElement.innerText === '') {
+                board[parseInt(e.target.id[1])] = playerIcon;
+                
+                //minimax stuff
+                bestMove(board);
+            }
+            //console.log(e.srcElement.innerText)
         }
-        //console.log(e.srcElement.innerText)
+        //for pvp stuff
+        else if (winner === null && gameMode === 'pvp') {
+            if (isEven(gameTurn)) {
+                board[parseInt(e.target.id[1])] = aiIcon;
+                gameTurn++;
+            }
+            else {
+                board[parseInt(e.target.id[1])] = playerIcon;
+                gameTurn++;
+            }
+        }
     })
+}
+
+const isEven = (num)=>{
+    if (num%2===0) {return true;}
+    else {return false;}
+}
+
+
+let gameMode = 'player';
+playerBtn.addEventListener('click', ()=>{
+    gameMode = 'player';
+    resetBoard();
+    gameTurn = 1;
+    document.querySelector('#result').style.display = 'none';
+    container.style.backgroundColor = 'cadetblue';
+
+    pvpBtn.classList.remove('active');
+    aiBtn.classList.remove('active');
+    playerBtn.classList.add('active');
+})
+
+aiBtn.addEventListener('click', ()=>{
+    gameMode = 'ai';
+    resetBoard();
+    gameTurn = 1;
+    document.querySelector('#result').style.display = 'none';
+    container.style.backgroundColor = 'cadetblue';
+
+    pvpBtn.classList.remove('active');
+    playerBtn.classList.remove('active');
+    aiBtn.classList.add('active');
+})
+
+pvpBtn.addEventListener('click', ()=>{
+    gameMode = 'pvp';
+    resetBoard();
+    gameTurn = 1;
+    document.querySelector('#result').style.display = 'none';
+    container.style.backgroundColor = 'cadetblue';
+
+    pvpBtn.classList.add('active');
+    playerBtn.classList.remove('active');
+    aiBtn.classList.remove('active');
+})
+
+let _ = 0;
+const aiMoveFirst = ()=>{ //make ai go first if AI start first is selected
+    if (!_) {
+        if (aiBtn.classList[0] === 'active') {
+            resetBoard();
+            bestMove(board); 
+            _++;
+            console.log(_)
+        }
+    }
+}
+
+const showRetry = (result)=>{
+    document.querySelector('#result').style.display = 'block';
+    if (gameMode !== 'pvp') {
+        if (result === 'player') {
+            retryText.innerText = 'You win!';
+        }
+        else if (result === 'ai') {
+            retryText.innerText = 'You lost!';
+        }
+        else {
+            retryText.innerText = "It's a draw!";
+        }
+    }
+    else if (gameMode === 'pvp') {
+        if (result === 'player') {
+            retryText.innerText = 'Player 1 wins!';
+        }
+        else if (result === 'ai') {
+            retryText.innerText = 'Player 2 wins!';
+        }
+        else {
+            retryText.innerText = "It's a draw!";
+        }
+    }
+}
+
+const resetBoard = ()=>{
+    board = [];
+    for (let i=0; i<9; i++) {board.push('');}
 }
 
 const updateUiFromBoard = ()=>{
@@ -106,7 +214,6 @@ const gameWinner = ()=>{
 
 //minimax
 
-let moveIndex;
 const minimaxScores = { //to set a score for each outcome. keys are the return values of gameWinner()
     'player': -1,
     'ai': 1,
@@ -114,6 +221,7 @@ const minimaxScores = { //to set a score for each outcome. keys are the return v
 };
 
 function bestMove(tempBoard){
+    let moveIndex;
     let bestScore = -Infinity;
     for (let i=0; i<tempBoard.length; i++) {
         if (tempBoard[i] === '') { //if this spot is free
@@ -131,7 +239,7 @@ function bestMove(tempBoard){
 
 function minimax(tempBoard, depth, isMaximizing) {
     winner = gameWinner();
-    if (winner !== null) { //if game has ended
+    if (winner !== null) { //if max-depth reached
         return minimaxScores[winner];
     }
     else {
